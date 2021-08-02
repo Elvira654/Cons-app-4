78d78029d7a2f4be76a8e4660ee885297c7a51ef
Elvira <57684716+Elvira654@users.noreply.github.com>
2021-08-02 14:32:42 +0200

    Add files via upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Sportista je planirao da u toku n dana priprema prvi dan pretrči a kilometara a sledećeg 10%
+više od norme predhodnog dana. Napisati program koji prikazuje koliko sportista treba da
+pretrči svakog dana u toku priprema, kao i koliko će ukupno pretrčati u toku priprema*/
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, a;
+            Console.WriteLine("Broj pripremnih dana: ");
+            n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Sportista prvi dan pretrci ? - ");
+            a = double.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Sportista je prvi dan pretrcao " + a + " kilometara.");
+                a *= 1.1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
